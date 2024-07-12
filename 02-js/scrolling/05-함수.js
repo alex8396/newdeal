835f550c0534c218407b3f0e0eb5a939c7186c1c
alex8396 <alex8396@naver.com>
2024-07-12 14:39:44 +0900

    dd

---
+++ 02-js/scrolling/05-함수.js
@@ -0,0 +1,20 @@
+// // 함수 선언하기
+// function 함수이름(매개변수1, 매개변수2) {
+//     // 코드작성
+//     return 결과값;
+// }
+// 함수이름(인자1,인자2);
+
+function hello1() {
+    console.log('hello, '+name+'!');
+}
+
+hello1('김가을'); // hello,김가을!
+
+
+// 함수 표현식
+const hello2 = function() {
+    console.log('아 내일 쉰다');
+};
+
+hello2(); // 아 내일 쉰다
