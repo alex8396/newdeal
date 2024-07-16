3d51697e351d8a0adf494c5f32cf11429f97e88b
alex8396 <alex8396@naver.com>
2024-07-16 09:45:50 +0900

    sadad

--- 02-js/06-클래스.js
+++ 02-js/06-클래스.js
@@ -32,10 +32,70 @@ const americano = {
 const latte = {
     name: '라떼',
     price: 4500,
-    orderCoffee() {
-        return `${this.name}는 ${this.price}원 입니다.`;
-    },
 };
 
 console.log(americano.orderCoffee()); // 아메리카노는 4000원 입니다.
-console.log(americano.orderCoffee(latte)); // 라떼는 4500원 입니다.
+console.log(americano.orderCoffee.call(latte)); // 라떼는 4500원 입니다.
+
+// prototype을 이용한 메서드 재활용
+const americano2 = new OrderCoffee('아메리카노', 4000);
+const latte2 = new OrderCoffee('라떼', 4500);
+
+function OrderCoffee(name, price) {
+    this.name = name;
+    this.price = price;
+}
+
+OrderCoffee.prototype.printOrder = function () {
+    return `${this.name}는 ${this.price}원 입니다.`;
+};
+
+console.log(americano2.printOrder()); // 아메리카노는 4000원 입니다.
+console.log(latte2.printOrder()); // 라떼는 4500원 입니다.
+
+// es6 class 문법을 이용한 생성자 함수
+class OrderCoffee2 {
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    printOrder() {
+        return `${this.name}는 ${this.price}원 입니다.`;
+    }
+
+    making() {
+        return `${this.name}를 만들고 있습니다.`;
+    }
+
+    made() {
+        return `${this.name}가 완성되었습니다.`;
+    }
+}
+
+class CharacterCoffee extends OrderCoffee2 {
+    constructor(name, price, character) {
+        super(name, price);
+        this.character = character;
+    }
+
+    printOrder() {
+        return `${this.character} ${this.name}는 ${this.price}원입니다.`;
+    }
+}
+
+// 객체 생성을 위한 클래스
+const americano3 = new OrderCoffee2('아메리카노', 4000);
+const latte3 = new OrderCoffee2('라떼', 4500);
+
+console.log(americano3.printOrder()); // 아메리카노는 4000원 입니다.
+console.log(latte3.printOrder()); // 라떼는 4500원 입니다.
+
+console.log(americano3.making()); // 아메리카노를 만들고 있습니다.
+console.log(latte3.making()); // 라떼를 만들고 있습니다.
+
+console.log(americano3.made()); // 아메리카노가 완성되었습니다.
+console.log(latte3.made()); // 라떼가 완성되었습니다.
+
+const charLatte = new CharacterCoffee('라떼', 6500, '미니언즈');
+console.log(charLatte.printOrder()); // 미니언즈 라떼는 6500원입니다.
